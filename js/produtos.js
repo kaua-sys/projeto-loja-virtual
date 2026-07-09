0db9ee31b06443befed53eb0d579200cd6a1c960
Luiz Kauã <lkaua4546@gmail.com>
2026-07-09 16:48:09 -0300

    Carregado os cards do html

---
+++ js/produtos.js
@@ -0,0 +1,42 @@
+//Importando os produtos do arquivo lista_produtos.js
+import { produtos } from "./lista_produtos.js";
+
+//Pegando elementos do DOM
+const sectionCards = document.querySelector('#cards')
+
+//Carregando os cards
+const lista_produtos = () =>{
+    sectionCards.innerHTML = ''
+ 
+    produtos.forEach((elem, i)=>{
+    const divCard = document.createElement('div')
+    divCard.setAttribute('class', 'card')
+
+    const imgCard = document.createElement('img')
+    imgCard.setAttribute('src',elem.caminho_imagem)
+    imgCard.setAttribute('alt', elem.descricao_produto)
+
+    const pCard = document.createElement('p')
+    pCard.innerHTML = elem.descricao_produto
+
+    const h2Card = document.createElement ('h2')
+    h2Card.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.',',')}`
+
+    const btnCard = document.createElement('button')
+    btnCard.setAttribute('class', 'btn-add')
+    btnCard.innerHTML = 'Adicionar'
+
+    divCard.appendChild(imgCard)
+    divCard.appendChild(pCard)
+    divCard.appendChild(h2Card)
+    divCard.appendChild(btnCard)
+    
+    sectionCards.appendChild(divCard)
+
+
+    })
+
+
+}
+
+lista_produtos()
