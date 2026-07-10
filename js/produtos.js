b404bc5eacc6073aa639afe26696db39b6434a44
Luiz Kauã <lkaua4546@gmail.com>
2026-07-10 13:47:28 -0300

    Adicionado comentários ao produtos.js

--- js/produtos.js
+++ js/produtos.js
@@ -6,31 +6,40 @@ const sectionCards = document.querySelector('#cards')
 
 //Carregando os cards
 const lista_produtos = () =>{
+    //Limpando o section cards
     sectionCards.innerHTML = ''
  
+    //Pecorrendo o array de produtos
     produtos.forEach((elem, i)=>{
+        //Criando o elemento div e definindo o atributo card
     const divCard = document.createElement('div')
     divCard.setAttribute('class', 'card')
 
+    //Criando o elemento img e definindo os atributos sc e alt os valores do caminho das imagens e a descrição dos produtos
     const imgCard = document.createElement('img')
     imgCard.setAttribute('src',elem.caminho_imagem)
     imgCard.setAttribute('alt', elem.descricao_produto)
 
+    //Criando o elemento p e atribuido a descrição dos produtos
     const pCard = document.createElement('p')
     pCard.innerHTML = elem.descricao_produto
 
+    //Criadno o elemento h2 e atribuindo o valor unitário deixando em duas casas decimais e substituindo ponto por vírgulo
     const h2Card = document.createElement ('h2')
     h2Card.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.',',')}`
 
+    //Criando o elemento button e definindo os atributos class e a descrição adicionar
     const btnCard = document.createElement('button')
     btnCard.setAttribute('class', 'btn-add')
     btnCard.innerHTML = 'Adicionar'
 
+    //Adicionando os elementos filhos aos divCard
     divCard.appendChild(imgCard)
     divCard.appendChild(pCard)
     divCard.appendChild(h2Card)
     divCard.appendChild(btnCard)
     
+    //Adicionando o divCard a section cards
     sectionCards.appendChild(divCard)
 
 
@@ -39,4 +48,5 @@ const lista_produtos = () =>{
 
 }
 
+//Chamando a Função listarProdutos
 lista_produtos()
