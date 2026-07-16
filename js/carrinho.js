6f7d834266bcd79fb0e55d970edc986385313ce1
Luiz Kauã <lkaua4546@gmail.com>
2026-07-16 16:36:23 -0300

    Adicioanado nova função chamada upadateQuantidade e adicionado novos comentarios

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,48 +1,85 @@
-//Criadno o array de itens do carrinho
+//Criando o array de itens do carrinho
 const itensCarrinho = JSON.parse(sessionStorage.getItem('carrinhoSessao')) || []
-//const itensCarrinho2 = JSON.parse(localStorage.get('carrinhoSessao')) || []
 
-//Função item
+//Função que cria um item para o carrinho
 const item = (objProduto) => {
-    const item ={
-    id_compra: objProduto.id_compra,
-    descricao_produto: objProduto.descricao_produto,
-    valor_unitario: objProduto.valor_unitario,
-    unidade: objProduto.unidade,
-    caminho_imagem: objProduto.caminho_imagem,
-    quantidade: '1'
+    const item = {
+        id_compra: objProduto.id_compra,
+        descricao_produto: objProduto.descricao_produto,
+        valor_unitario: objProduto.valor_unitario,
+        unidade: objProduto.unidade,
+        caminho_imagem: objProduto.caminho_imagem,
+        quantidade: 1
     }
 
     return item
-} 
-
-//Criando para que o usúario adicione mais 1 na quantidade quando repetir a escolha do instrumento
-const itemAdd = (add) =>{
-    
 }
 
-//Função para adicionar um item
-const addItem = (objItem)=>{
+//Verificando se o produto já existe no carrinho
+const itemAdd = (add) => {
+    const itemCarrinho = itensCarrinho.findIndex(
+        elem => elem.id_compra == add.id_compra
+    )
+
+    //Aumentando a quantidade do produto
+    if (itemCarrinho != -1) {
+        itensCarrinho[itemCarrinho].quantidade++
+
+        //Atualizando o sessionStorage
+        sessionStorage.setItem(
+            'carrinhoSessao',
+            JSON.stringify(itensCarrinho)
+        )
+    } else {
+        //Adicionando um novo produto
+        addItem(item(add))
+    }
+}
+
+//Função para adicionar um item ao carrinho
+const addItem = (objItem) => {
     itensCarrinho.push(objItem)
 
-    sessionStorage.setItem('carrinhoSessao', JSON.stringify(itensCarrinho))
+    //Salvando os dados no sessionStorage
+    sessionStorage.setItem(
+        'carrinhoSessao',
+        JSON.stringify(itensCarrinho)
+    )
 }
 
 //Função para listar os itens do carrinho
-const listItens = () =>{
+const listItens = () => {
     const listaItens = JSON.parse(sessionStorage.getItem('carrinhoSessao'))
 
     return listaItens
 }
 
-// Função para remover um item
+//Função para remover um item do carrinho
 const removeItem = (indice) => {
-    itensCarrinho.splice(indice, 1);
+    itensCarrinho.splice(indice, 1)
 
+    //Atualizando o sessionStorage
     sessionStorage.setItem(
         "carrinhoSessao",
         JSON.stringify(itensCarrinho)
-    );
+    )
 }
 
-export { addItem, listItens, removeItem };
+//Função para atualizar a quantidade de um produto
+const updateQuantidade = (idCompra, quantidade) => {
+
+    //Localizando o produto no carrinho
+    const indice = itensCarrinho.findIndex(elem => elem.id_compra == idCompra)
+
+    if (indice != -1) {
+        itensCarrinho[indice].quantidade = quantidade
+
+        //Salvando a nova quantidade
+        sessionStorage.setItem(
+            'carrinhoSessao',
+            JSON.stringify(itensCarrinho)
+        )
+    }
+}
+
+export { itemAdd, listItens, removeItem, updateQuantidade }
